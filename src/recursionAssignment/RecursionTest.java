fbe561fd3e2377587f4ed510d84b7ab6f7baf6bb
Shar <Sharungan.Umaipalan20@ycdsbk12.ca>
2020-12-17 03:38:16 +0000

    Test 1 red

--- src/recursionAssignment/RecursionTest.java
+++ src/recursionAssignment/RecursionTest.java
@@ -1,34 +1,36 @@
 package recursionAssignment;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Before;
-import org.junit.jupiter.api.After;
+import org.junit.Before;
+import org.junit.After;
 
 import recursionAssignment.Recursion;
 
 public class RecursionTest{
-    
-    @Before
-    public void beforeTest(){
-      // some steps to perform before tests are run
-    }
     
     /**
      * An initial test
      */
     @Test
     public void Test1(){
-        // make assertion statement(s)
-        // assertEquals(EXPECTEDVALUE, TESTVALUE);
-        // assertTrue(TESTBOOLEAN);
+      // make assertion statement(s)
+      // assertEquals(EXPECTEDVALUE, TESTVALUE);
+      // assertTrue(TESTBOOLEAN);
+      assertEquals(2, Recursion.count7(717));
     }
-    
-    // add more tests
-    
-    @After
-    public void afterTest(){
-      // some steps to perform after tests are run
+    public void Test2(){
+      assertEquals(1, Recursion.count7(7));
     }
-    
-    
+    public void Test3(){
+      assertEquals(0, Recursion.count7(123));
+    }
+    public void Test4(){
+      assertEquals("xxre",Recursion.endX("rexx"));
+    }
+    public void Test5(){
+      assertEquals("xxhixx",Recursion.endX("hixxxx"));
+    }
+    public void Test6(){
+      assertEquals("xhixhix",Recursion.endX("hihixxx"));
+  }
 }
