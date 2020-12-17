04b9621b854e9dff6293b770effc341ed099da41
Shar <Sharungan.Umaipalan20@ycdsbk12.ca>
2020-12-17 03:53:39 +0000

    Green for count7

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -2,19 +2,18 @@ package recursionAssignment;
 
 public class Recursion{
 
-  //public static int count7(int n){
-    //if(n == 0){
-      //return 0;
+  public static int count7(int n){
+    if(n == 0){
+      return 0;
       //checks if the number is zero, if it is return 0
-    //}else if(n%10 == 7){
-      //return 1 + count7(n/10);
+    }else if(n%10 == 7){
+      return 1 + count7(n/10);
       //checks if the number divided by 10 will leave the rightmost number to have a remainder of 7, if it does return 1 and divide the number by 10
-    //}else{
-      //return 0 + count7(n/10);
-      //return 0 if the number divided by 10 doesn't have a remainder of 7 and add the number divided by 10
-      //this will continue until n=0 and then the all the returns will be added together
-   // }
-//  }
+    }else{
+      return 0 + count7(n/10);
+      //return 0 if the number divided by 10 doesn't have a remainder of 7 and add the number divided by 10 this will continue until n=0 and then the all the returns will be added together
+    }
+  }
   //public static String endX(String str){
     //if(str.substring(0,1).equals("")){
       //return endX(str);
